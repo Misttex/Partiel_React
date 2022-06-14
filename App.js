31d28ed0224f543dd524d8be679da56b7e56b310
Misttex <magalhaes.quentin@free.fr>
2022-06-14 16:10:55 +0200

    step 3

--- App.js
+++ App.js
@@ -1,35 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View, Image, FlatList, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, SafeAreaView, FlatList, listExpression, renderItem } from 'react-native';
 import React, { useState, useEffect } from 'react';
-import {TouchableOpacity} from "react-native";
-import {SafeAreaView} from "react-native";
-
+import { TouchableOpacity } from 'react-native';
 
 export default function App() {
+    const [character, setCharacter] = useState(null);
+    const [quote, setQuote] = useState(null);
+    const [icon, setIcon] = useState(null);
+    const [input, setInput] = useState("");
+    const [listQuote, setlistQuote] = useState(null);
 
-    //Use state pour step 1
-    const [character1, setCharacter] = useState(null);
+    const ItemSaisir = (data) => (
+        <View>
+            <View style={styles.containerGlobal}>
+                <Image
+                    style={styles.image}
+                    source={{uri: data.icon}}/>
+                <View>
+                    <Text style={styles.text}>{data.character}</Text>
+                    <Text style={styles.text}>{data.quote}</Text>
+                </View>
+            </View>
+        </View>
+    );
 
-    const [quote1, setQuote] = useState(null);
+    const renderItem = ({item}) => (
+        <TouchableOpacity><ItemSaisir data={item} /></TouchableOpacity>
+    );
 
-    const [icon1, setIcon] = useState(null);
-
-    //UseState pour step 2
-    const [listExpression, setListeExpression] = useState(null);
-
-    const [number,setNumber] = useState(0);
-
-    //Use state pour step 3
-    const [character2, setCharacter2] = useState(null);
-
-    const [quote2, setQuot2] = useState(null);
-
-    const [icon2, setIco2] = useState(null);
-
-    const [nomSimpson, setNomSimpson] = useState("");
 
     const getNomSimpson = () => {
-        return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?character=' + nomSimpson)
+        return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?character=' + input)
             .then((response) => response.json())
             .then((json) => {
                 setCharacter('Nom: ' + json[0].character)
@@ -45,80 +46,23 @@ export default function App() {
     }
 
 
-    const getSimpSon = () => {
-        return fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
-            .then((response) => response.json())
-            .then((data) => {
-
-                setCharacter('Nom: ' + data[0].character)
-                setQuote('Citation: ' + data[0].quote)
-                setIcon(data[0].image)
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    };
-    const getSimpSonExpression = () => {
-        return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count='+nomSimpson)
-            .then((response) => response.json())
-            .then((data) => {
-                setListeExpression(data)
-                //console.log(data)
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    };
-
     useEffect(() => {
         (async () => {
-            getSimpSon();
+            // getNomChar();
         })();
     }, []);
 
-    const Item = () => (
-        <View>
-            <View style={styles.containerGlobal}>
-                <Image
-                    style={styles.image}
-                    source={{uri: icon1}}/>
-                <View>
-                    <Text style={styles.text}>{character1}</Text>
-                    <Text style={styles.text}>{quote1}</Text>
-                </View>
-            </View>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => getSimpSon()}>
-                <Text style={styles.textButton}>Reload</Text>
-            </TouchableOpacity>
-        </View>
-    );
-
-    const ItemSaisir = (data) => (
-        <View>
-            <View style={styles.containerFlatList}>
-                <Image
-                    style={styles.imageExpression}
-                    source={{uri: data.data.image}}/>
-                <View>
-                    <Text style={styles.textFlatList}>Nom : {data.data.character}</Text>
-                    <Text style={styles.textFlatList}>Description : {data.data.quote}</Text>
-                </View>
-            </View>
-        </View>
-    );
-
-    const ItemStep3 = (data) => (
+    return (
         <View style={styles.container}>
-            <View>
-                <TextInput placeholder="Entrez un nom" onChangeText={item => setNomSimpson(item)}/>
+            <View style={styles.recherche}>
+                <TextInput style={styles.input} placeholder="Entrez un nom" onChangeText={item => setInput(item)} clearButtonMode/>
                 <TouchableOpacity
+                    style={styles.button2}
                     onPress={() => {
                         getNomSimpson();
                     }}
                 >
-                    <Text>Chercher</Text>
+                    <Text>Rechercher</Text>
                 </TouchableOpacity>
                 <SafeAreaView style={styles.container}>
                     <View style={styles.textcarte}>
@@ -126,85 +70,95 @@ export default function App() {
                 </SafeAreaView>
             </View>
 
-            <View>
-                <View >
+            <View style={styles.carte}>
+                <View style={styles.carte2}>
                     <Image
                         style={styles.tinyLogo}
-                        source={{uri: icon2}}
+                        source={{uri: icon}}
                     />
                     <View style={styles.textcarte}>
-                        <Text style={styles.text2}>{character2}</Text>
-                        <Text style={styles.text2}>{quote2}</Text>
+                        <Text style={styles.text2}>{character}</Text>
+                        <Text style={styles.text2}>{quote}</Text>
                     </View>
                 </View>
             </View>
         </View>
     );
-
-    const renderItem = ({item}) => (
-        <TouchableOpacity><ItemSaisir data={item} /></TouchableOpacity>
-    );
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <Item/>
-            <View >
-                <Text >Saisir un nombre</Text>
-                <TextInput placeholder={"Saisir le nombre d'expression à afficher"} editable maxLength={15} onChangeText={item=>setNumber(item)} clearButtonMode/>
-                <View>
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={getSimpSonExpression}
-                    >
-                        <Text style={styles.textButton}>Valider</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <SafeAreaView style={styles.container}>
-                <FlatList data={listExpression} renderItem={renderItem}/>
-            </SafeAreaView>
-        </SafeAreaView>
-    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-    containerGlobal:{
-      backgroundColor:'#DAF7A6',
-        padding:20,
-        borderRadius:15,
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    containerFlatList:{
-        backgroundColor:'#DAF7A6',
-        borderRadius:15,
-        padding:25,
+    separator: {
+        paddingBottom: 20
     },
-    button:{
+    input: {
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 25
+    },
+    recherche: {
+        alignContent: 'center',
+        paddingLeft: 80,
+        paddingBottom: 15,
+        flexDirection: "row",
+    },
+    tinyLogo: {
+        width: 106,
+        height: 200,
+        marginLeft: 10,
+        marginRight: 20
+    },
+    samelesbrise: {
+        alignItems: "center"
+    },
+    button: {
         padding: 10,
+        width: 80,
         backgroundColor: 'orange',
         marginVertical:10,
         borderRadius: 10,
+        borderWidth: 2,
+        alignItems: "center",
+        alignContent: "center"
     },
-    image: {
-        width: 106,
-        height: 200,
-    }, imageExpression: {
-        width: 90,
-        height: 120,
+    button2: {
+        margin: 15,
+        width: 100,
+        backgroundColor: 'orange',
+        marginVertical:10,
+        borderRadius: 10,
+        padding: 10,
+        alignItems: "center",
+        alignContent: "center"
     },
-    text:{
-        fontSize:21,
+    carte: {
+        flexDirection: "column",
+        paddingLeft: 5
     },
-    textFlatList:{
-        fontSize:12,
+    carte2: {
+        flexDirection: 'row',
+        borderWidth: 2,
+        width: 400,
+        backgroundColor: "pink",
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+
     },
-    textButton:{
-      fontSize:21,
-        textAlign:'center',
+    textcarte: {
+        flexDirection: 'column',
+        width: 250,
+        justifyContent: "center",
+    },
+    text2: {
+        paddingBottom: 10,
+        paddingTop: 10
     }
+
 });
